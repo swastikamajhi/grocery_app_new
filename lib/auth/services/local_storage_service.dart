01b9971421506f96cfa755ca1f766a4bba57fbdb
rahul-velocis <rahul@velociscore.com>
2025-06-30 18:12:20 +0545

    Added some basic method declaration in the local storage services

--- lib/auth/services/local_storage_service.dart
+++ lib/auth/services/local_storage_service.dart
@@ -1,3 +1,17 @@
-class LocalStorageService {
-  
+import 'package:new_app/models/user_model.dart';
+
+abstract class LocalStorageService {
+  final bool isLoggedIn = false;
+
+  Future<void> saveUserData(UserModel model);
+
+  Future<UserModel> getUserData(UserModel model);
+
+  Future<bool> userLoggedIn();
+  //if user is logged in, true, navigate to homepage
+  // if user if not logged in ,false, navigate to login page
+
+  Future<void> logoutUser() {}
+
+  Future<void> loginUser() {}
 }
